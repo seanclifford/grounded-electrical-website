7a8d3ea79470f5265f580865a67038bd608820c2
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2022-05-07 20:46:06 +1200

    Improve menu item appearance. Introduce main image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'geblue': '#17194a'
+      }
+    },
     fontFamily: {
       'sans': ['Montserrat', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
       'vag-rounded' : ['VAGRoundedBT', 'sans-serif']
